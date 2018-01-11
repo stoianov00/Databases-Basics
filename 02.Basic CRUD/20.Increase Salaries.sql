3edd7f0956d3d48f784f1f7ed357d312e3f0aa17
Konstantin Stoianov <stoianovkonstantin00@gmail.com>
2018-01-11 18:12:33 +0200

    Add files via upload

---
+++ 02.Basic CRUD/20.Increase Salaries.sql
@@ -0,0 +1,19 @@
+UPDATE Employees
+SET Salary *= 1.12
+WHERE DepartmentID IN (
+	SELECT DepartmentID FROM Departments
+	WHERE Name IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+)
+
+SELECT Salary FROM Employees
+
+-- Restore DB to revert changes
+
+UPDATE Employees
+SET Salary /= 1.12
+WHERE DepartmentID IN (
+	SELECT DepartmentID FROM Departments
+	WHERE Name IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+)
+
+SELECT Salary FROM Employees
---
+++ 02.Basic CRUD/23.Countries and Currency Euro Not Euro.sql
@@ -0,0 +1,5 @@
+SELECT CountryName, CountryCode, 
+	IIF(Currencies.CurrencyCode = 'EUR', 'Euro', 'Not Euro') AS Currency
+FROM Countries 
+LEFT JOIN Currencies ON Currencies.CurrencyCode = Countries.CurrencyCode
+ORDER BY CountryName
